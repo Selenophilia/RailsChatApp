99830776afaced707319242437d75db2cb1064ee
Selenophilia <pauloargenal@gmail.com>
2020-09-15 19:07:54 +0800

    Added new validations for email and new tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true 
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+                 message: "Email invalid"  },uniqueness: { case_sensitive: false },
+                length: { minimum: 4 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,14 +3,42 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   setup do
     @user = users(:user)
-   # byebug
   end
 
-  test "should be valid" do
+  test "should not save if email format is incorrect" do
+    @user.email = "emailemail.com"
+    assert_match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, "sample@email.com", "Email invalid" )
+    assert_not @user.save, "Saved the email field with incorrect format"
+  end
+  
+  test "email  should have a minimum of atleast 4 characters" do
+    @user.email = "na@email.com"
+    assert_not_equal [@user.email], 'email@email.com', "Invalid email, email should have atleast 4 char"
+  end
+
+  test "should be valid if email format is correct" do
     assert @user.valid?
   end
 
-  test "name should be present" do
+  test "email should be unique" do
+    duplicate_item = @user.dup
+    @user.save(validate: false)
+    duplicate_item.valid? 
+    assert duplicate_item.errors
+    assert duplicate_item.errors[:username]
+end
+  
+  test "email should not save if not present during sign up" do
+    @user.email = "" 
+    assert_not @user.save
+  end
+
+  test "password should not save if not present during sign up" do
+    @user.password = ""
+    assert_not @user.save
+  end
+  
+  test "email should be present" do
     @user.email = "email@email.com"
     assert @user.valid?
   end
