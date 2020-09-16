7bcba78f6649c112a5725b80f557edcf7891921e
Selenophilia <pauloargenal@gmail.com>
2020-09-16 21:42:38 +0800

    Added routes and modified user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'users/messages', to: 'message#index'
+  get 'users/:user_id/messages/new' => 'message#new', as: 'new_message'
+  post 'users/:user_id/messages/messages'    => 'message#create', as: 'create_message'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_122923) do
+ActiveRecord::Schema.define(version: 2020_09_16_113056) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "users_id"
+    t.string "message_body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_messages_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
