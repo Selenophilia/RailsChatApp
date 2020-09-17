2a0a5a4a134c18b3938770da5d97856e441ed3a8
Selenophilia <pauloargenal@gmail.com>
2020-09-17 22:27:57 +0800

    modified chatroom and message foreign key contraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_071805) do
+ActiveRecord::Schema.define(version: 2020_09_17_125229) do
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_chatrooms_on_name", unique: true
+  end
 
   create_table "messages", force: :cascade do |t|
     t.integer "users_id"
     t.string "message_body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "chatroom_id"
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["users_id"], name: "index_messages_on_users_id"
   end
 
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_09_17_071805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "chatrooms"
 end
