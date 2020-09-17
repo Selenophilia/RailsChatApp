912e5d6c5526d10e56f58e92eac93864e6f441f1
Selenophilia <pauloargenal@gmail.com>
2020-09-17 13:02:23 +0800

    Added resources routes for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+       sessions: 'users/sessions', 
+       registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'users/messages', to: 'message#index'
-  get 'users/:user_id/messages/new' => 'message#new', as: 'new_message'
-  post 'users/:user_id/messages/messages'    => 'message#create', as: 'create_message'
+  root controller: :chatroom, action: :index
 
+  resources :chatroom
+  resources :messages
 end
