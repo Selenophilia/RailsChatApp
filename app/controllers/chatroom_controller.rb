b5cc099714d8e8db075921df121fca5559a668fe
Selenophilia <pauloargenal@gmail.com>
2020-09-17 16:22:14 +0800

    Moved the create message in chatroom controller

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,9 +1,21 @@
 class ChatroomController < ApplicationController    
     def index
-    
+        @messages = Message.all
+    end
+
+    def new
+        @message = Message.new
     end
 
     def create
-    
-    end 
+        @message = Message.create(params.permit(:message_body, :user_id))
+        byebug
+        if @message.save               
+            redirect_to '/messages' 
+
+        else
+
+            redirect_to new_message_url    
+        end 
+    end
 end
--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,22 +1,3 @@
 class MessageController < ApplicationController
-    before_action :authenticate_user!
-    def index
-        @messages = Message.all
-    end
 
-    def new
-        @message = Message.new
-    end
-
-    def create
-        @message = Message.create(params.permit(:message_body, :user_id))
-        byebug
-        if @message.save               
-            redirect_to '/messages' 
-
-        else
-
-            redirect_to new_message_url    
-        end 
-    end
 end
