f542cb883d57615f9de563ad05cd0f818f2f8815
Selenophilia <pauloargenal@gmail.com>
2020-09-18 22:51:16 +0800

    Removed before action method

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,4 @@
 class ChatroomController < ApplicationController    
-   before_action :load_chat_rooms, only: [:show ]
-
     def index        
         @messages = Message.all
         @message_new = Message.new
