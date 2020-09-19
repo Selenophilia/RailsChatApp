188ce2167be3b391456eaa04e8070b5f4bc86de4
Selenophilia <pauloargenal@gmail.com>
2020-09-19 17:46:06 +0800

    Added a code in chatroom_channel to always stay at bottom when sending a message

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -9,10 +9,14 @@ consumer.subscriptions.create("ChatroomChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received(data) {
-    let input = document.querySelector('input[type="text"]')
+  received(data) {  
+    let obj = document.querySelector('#message-container')
+
+    let scroll_bottom = document.querySelector('#message-container').scrollHeight
+    
     // Called when there's incoming data on the websocket for this channel
     $('#message-container').append(data.mod_message)
-    $('#message_message_body').val('')
+    $('#message_message_body').val('');
+    obj.scrollTop = scroll_bottom;
   }
 });
