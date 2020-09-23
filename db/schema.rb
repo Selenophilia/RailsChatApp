d5990ddeb29b622965106defa7fa14a9f7135933
Selenophilia <pauloargenal@gmail.com>
2020-09-23 22:45:52 +0800

    Modified show.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_092718) do
+ActiveRecord::Schema.define(version: 2020_09_23_134212) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
